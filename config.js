655ef84d5d5e6db24c681e1396fbf77e403180b5
ajb1102 <abiss29@gmail.com>
2019-01-05 12:38:54 -0500

    added functions

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.ACCESS_TOKEN = "dWFgwPXAm8EB3w6k9Tm4qmneNFpWTJXXfkdcZbTH"; //andy's personal access token
+exports.BOT_ID = "971616c58b3080db2bad4c8a97";
+exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
+exports.GIPHY_API_KEY = "IkjDQqF63VbgHHarei42X0QctIwCP4kG"; //used to be export and ' instead of "
+exports.GROUP_ID = "30079020";
