cec27888ff988f91e7ce4fd9f082c9f9db8bc08f
ajb1102 <abiss29@gmail.com>
2020-01-07 00:52:59 -0500

    Delete config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-exports.ACCESS_TOKEN = "dWFgwPXAm8EB3w6k9Tm4qmneNFpWTJXXfkdcZbTH"; //andy's personal access token
-exports.BOT_ID = "e36360d5fcde52811b61b51aa9"; //BOT TEST: e36360d5fcde52811b61b51aa9      old:971616c58b3080db2bad4c8a97
-exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
-exports.GIPHY_API_KEY = "IkjDQqF63VbgHHarei42X0QctIwCP4kG"; //used to be export and ' instead of "
-exports.GROUP_ID = "50957419";
