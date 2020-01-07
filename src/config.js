e337442caa0571a3b75edfad1194969343a29d39
ajb1102 <abiss29@gmail.com>
2020-01-07 01:25:38 -0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+exports.ACCESS_TOKEN = "{Access token}";
+exports.BOT_ID = "{Bot ID}";
+exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
+export.GIPHY_API_KEY = '{Giphy key}'
+exports.GROUP_ID = "{Group ID}";
