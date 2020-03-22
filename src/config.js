df4acb929b3b38a22560b654b22e82c797e101fc
ajb1102 <abiss29@gmail.com>
2020-03-22 16:02:18 -0400

    updated to friend group

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 exports.ACCESS_TOKEN = "uy6AL6PHAXTDk66pRw3G83FJ2HX7k5TNkKDyRp7H";
-exports.BOT_ID = "e36360d5fcde52811b61b51aa9";
+//exports.BOT_ID = "e36360d5fcde52811b61b51aa9"; //test group
+exports.BOT_ID = "d6e7dd91b27539d937c430e89c"; //koopins
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 exports.GIPHY_API_KEY = "IkjDQqF63VbgHHarei42X0QctIwCP4kG";
-exports.GROUP_ID = "50957419";
+//exports.GROUP_ID = "50957419"; //test group
+exports.GROUP_ID = "30079020"; //koopins
