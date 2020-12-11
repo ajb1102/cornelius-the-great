15e42be5168c164a3a5c8cde55a6ea6b9369ca17
ajb1102 <abiss29@gmail.com>
2020-12-11 18:36:37 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,3 +5,4 @@ exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 exports.GIPHY_API_KEY = "IkjDQqF63VbgHHarei42X0QctIwCP4kG";
 //exports.GROUP_ID = "50957419"; //test group
 exports.GROUP_ID = "30079020"; //koopins
+// heroku BOT_ID: 971616c58b3080db2bad4c8a97
